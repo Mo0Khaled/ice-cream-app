2564ac3d060e23bb5a1a7478b4fea7766c80ed19
Mo <mohamedkx77@gmail.com>
2020-06-11 21:35:24 +0200

    edit title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class IceCreamApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Ice Cream",
+      title: "Ice Cream App",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.white),
       home: Home(),
